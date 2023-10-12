fb399aadca43414df07a3e83e1082cf1ccf9b8b0
Steven-c4 <117254511+Steven-c4@users.noreply.github.com>
2023-10-12 11:28:31 -0600

    menufinalizado

--- proyecto/Assets/scrips/MenuPausa.cs
+++ proyecto/Assets/scrips/MenuPausa.cs
@@ -1,26 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ManePausa : MonoBehaviour
 {
-    [SerializeField] private GameObject barraDeVida;
+    [SerializeField] private GameObject vida;
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
 
     public void Pausa()
     {
+        juegoPausado = true;
         Time.timeScale = 0f;
         botonPausa.SetActive(false);
         menuPausa.SetActive(true);
-        barraDeVida.SetActive(false); // Oculta la barra de vida cuando se pausa.
+        OcultarBarraDeVida();
     }
 
     public void Reanudar()
     {
+        juegoPausado = false;
         Time.timeScale = 1f;
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
-        barraDeVida.SetActive(true); // Muestra la barra de vida cuando se reanuda.
+        MostrarBarraDeVida();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+
+    public void OcultarBarraDeVida()
+    {
+        vida.SetActive(false);
+    }
+
+    public void MostrarBarraDeVida()
+    {
+        vida.SetActive(true);
+    }
+
+    public void Reiniciar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Cerrar()
+    {
+        Debug.Log("Cerrando juego");
+        Application.Quit();
+    }
+
+    public void IrAMenuInicial()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menuinicial"); 
     }
 }
